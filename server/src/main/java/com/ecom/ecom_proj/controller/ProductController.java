317b253603bef7f0c4a3cbd4209f193e216125ad
ZahaAnass <zahaanass277@gmail.com>
2025-12-11 19:47:38 +0100

    add search functionality

--- server/src/main/java/com/ecom/ecom_proj/controller/ProductController.java
+++ server/src/main/java/com/ecom/ecom_proj/controller/ProductController.java
@@ -90,4 +90,11 @@ public class ProductController {
         return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(String keyword) {
+        System.out.println("Searching products with keyword: " + keyword);
+        List<Product> products = service.searchProducts(keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
 }
